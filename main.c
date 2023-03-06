4619bd5215d16e330aaadadadcd5128707f5eb94
usernaresh <32897361+usernaresh@users.noreply.github.com>
2023-03-06 01:14:10 -0600

    structure of a simple state machine
    State transitions are as follows:
    State1 -> state2 on event1
    State2 -> state3 on event2
    State3 -> state1 on event3
    FYI: Error handlings/unexpected state transitions not handled

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+typedef enum state {
+    state1 =0,
+    state2 =1,
+    state3 =2,
+} state_t;
+
+typedef enum event {
+    e1,
+    e2,
+    e3,
+} event_t;
+
+void state1_actions(){
+    printf("\n in state1 actions \n");
+}
+
+state_t state2_actions(){
+    printf("\n in state2 actions \n");
+}
+
+state_t state3_actions(){
+    printf("\n in state3 actions \n");
+}
+
+typedef struct ds_state {
+    event_t event;
+    state_t current_state;
+    state_t next_state;
+    state_t (*state_method)();
+}ds_state_t;
+
+ds_state_t state_mc[] = { 
+    { e1, state1, state2, state1_actions},
+    { e2, state2, state3, state2_actions},
+    { e3, state3, state1, state3_actions},
+};
+
+void run_mc(event_t ev, state_t* st){
+    if(state_mc[ev].current_state == *st){
+        *st = state_mc[ev].next_state;
+        state_mc[ev].state_method(st);
+    }
+}
+
+int main()
+{
+    state_t st = state1;
+    event_t ev = e1;
+    run_mc(ev, &st); 
+    ev =e2;
+    run_mc(ev, &st);
+    ev=e3;
+    run_mc(ev, &st);
+    return 0;
+}
